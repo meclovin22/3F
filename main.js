98310f4d03c7d864c55725c5836aa8c8d8769480
guilhermeaugusto <110115887+meclovin22@users.noreply.github.com>
2024-06-14 08:53:14 -0300

    14/06

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date(2024,7)
+const tempoObjetivo2 = new Date(2024,8)
+const tempoObjetivo3 = new Date(2024,9)
+const tempoObjetivo4 = new Date(2024,10)
+
 
 
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length ; j++){
-            botoes[j].classList.remove("ativo")
-            conteudo[j].classList.remove("ativo")
+            botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo")
-        conteudo[i].classList.add("ativo")
+        botoes[i].classList.add("ativo");
+        conteudo[i].classList.add("ativo");
     }
 }

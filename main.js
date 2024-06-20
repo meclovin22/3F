768c04d57384067ce20fcd144c11fed7912a0a35
guilhermeaugusto <110115887+meclovin22@users.noreply.github.com>
2024-06-20 09:41:14 -0300

    20/06

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date(2024,7)
-const tempoObjetivo2 = new Date(2024,8)
-const tempoObjetivo3 = new Date(2024,9)
-const tempoObjetivo4 = new Date(2024,10)
-
+const tempoObjetivo1 = new Date(2024,7);
+const tempoObjetivo2 = new Date(2024,8);
+const tempoObjetivo3 = new Date(2024,9);
+const tempoObjetivo4 = new Date(2024,10);
+let agora = new Date ();
+contador[0].textContent = tempoObjetivo1-agora
+contador[1].textContent = tempoObjetivo2-agora
+contador[2].textContent = tempoObjetivo3-agora
+contador[3].textContent = tempoObjetivo4-agora
 
 
 for (let i = 0 ; i < botoes.length ; i++){
